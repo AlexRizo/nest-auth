835fc78f0291141019e124a0e2ab9c132b7c68cf
devalowee <david_alejandrorizo@outlook.com>
2026-07-02 12:51:31 -0600

    user module done

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -64,11 +64,59 @@ export class UsersService {
     return await this.prisma.user.findUnique({ where });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    await this.findOne(id);
+
+    if (updateUserDto.email) {
+      const emailExists = await this.findOneOrNull(updateUserDto.email);
+
+      if (emailExists && emailExists.id !== id)
+        throw new ConflictException(`El email ya existe`);
+    }
+
+    if (updateUserDto.username) {
+      const uName = await this.findOneOrNull(updateUserDto.username);
+
+      if (uName && uName.id !== id)
+        throw new ConflictException(`El nombre de usuario ya existe`);
+    }
+
+    try {
+      const userToUpdate = await this.prisma.user.update({
+        where: { id },
+        data: updateUserDto,
+      });
+
+      return userToUpdate;
+    } catch (error) {
+      this.logger.error(
+        'Ha ocurrido un error al actualizar el usuario:',
+        JSON.stringify(error),
+      );
+      throw new InternalServerErrorException(`Error al actualizar el usuario`);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const user = await this.findOne(id);
+
+    if (user.status === 'DELETED') {
+      throw new ConflictException(`El usuario ya ha sido eliminado`);
+    }
+
+    try {
+      await this.prisma.user.update({
+        where: { id },
+        data: { status: 'DELETED' },
+      });
+
+      return { ok: true };
+    } catch (error) {
+      this.logger.error(
+        'Ha ocurrido un error al eliminar el usuario:',
+        JSON.stringify(error),
+      );
+      throw new InternalServerErrorException(`Error al eliminar el usuario`);
+    }
   }
 }
