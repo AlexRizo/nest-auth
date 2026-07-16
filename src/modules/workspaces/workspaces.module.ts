9d5fbe88ff9170e132bbc2185fbccf99b5111be1
devalowee <david_alejandrorizo@outlook.com>
2026-07-16 12:28:50 -0600

    import prisma module

--- src/modules/workspaces/workspaces.module.ts
+++ src/modules/workspaces/workspaces.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [WorkspacesController],
   providers: [WorkspacesService],
+  exports: [WorkspacesService],
+  imports: [PrismaModule],
 })
 export class WorkspacesModule {}
