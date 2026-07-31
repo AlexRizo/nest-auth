7d2b5d91fc44deb14ae1f25d4458a8e81ee4716a
devalowee <david_alejandrorizo@outlook.com>
2026-07-31 12:50:30 -0600

    add comment

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './create-user.dto';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  // Aquí se agrega el workspaceId
   @IsString()
   @IsOptional()
   favoriteWorkspaceId?: string;
